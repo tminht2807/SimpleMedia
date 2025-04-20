79d77b75d503606c9d0d34fc915b720faa646a86
tminht2807 <tminht2807@gmail.com>
2025-04-20 16:28:04 +0700

    Error must run serial monitor before to allign the data sent?

--- Test_Read_From_Board/test_1.cpp
+++ Test_Read_From_Board/test_1.cpp
@@ -12,6 +12,8 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_mixer.h>
 
+// g++ test_1.cpp -o output `sdl2-config --cflags --libs` -lSDL2_mixer
+
 size_t index_play = 0;
 std::vector<std::string> list;
 Mix_Music* currentMusic;
@@ -19,28 +21,9 @@ bool isPlaying;
 std::string folder_path = "/home/tuan/Music/pop"; // Change this to your folder path
 
 
-void uartListener(int serial_port) {
-    while (true) {
-        uint8_t data; // 1-byte data bus
-        uint8_t volume;
-        uint8_t play;
-        int bytes_read = read(serial_port, &data, 1); // Read one byte
-
-
-        if (bytes_read > 0) { // Ensure full byte is received
-            volume = data & 0x7F;
-            play = data & 0x80;
-
-            std::cout << "Binary:\n";
-            std::cout << "    Total receive: " << std::bitset<8>(data) << std::endl;
-            std::cout << "    Volume:        " << std::bitset<8>(volume) << std::endl;
-            std::cout << "    Play:          " << std::bitset<8>(play) << std::endl;  
-
-            tcflush(serial_port, TCIFLUSH);
-        } 
-
-        usleep(20000); // Wait 20ms before reading again
-    }
+void ChangeVolume(uint8_t _vol){ // vol is range 0-63
+    int volume = static_cast<int>(_vol * (MIX_MAX_VOLUME / 64));
+    Mix_VolumeMusic(volume);
 }
 
 void Pause_Resume(){
@@ -58,8 +41,68 @@ void Next(){
     else index_play = 0;
     currentMusic = Mix_LoadMUS(list[index_play].c_str());
     Mix_PlayMusic(currentMusic, 1);
+    isPlaying = true;
     Mix_HookMusicFinished(Next);
 }
+
+void uartListener() {
+    bool isConnected = false;
+    const char* port = "/dev/ttyACM0"; // Adjust for your system
+    int serial_port;
+    struct termios tty;
+    
+    while (true) {
+        while (!isConnected) {
+            std::cout << "Waiting for connection..." << std::endl;
+            sleep(1);
+            // Check if the serial port is open
+            serial_port = open(port, O_RDWR);
+
+            if (serial_port >= 0) {
+
+                isConnected = true;
+                tcgetattr(serial_port, &tty);
+                cfsetspeed(&tty, B115200); // Set baud rate
+                tcsetattr(serial_port, TCSANOW, &tty);
+
+                std::cout << "Connected to serial port." << std::endl;
+            }
+        }
+    
+        uint8_t data; // 1-byte data bus
+        int bytes_read = read(serial_port, &data, 1); // Read one byte
+        
+        if (bytes_read > 0) { // Ensure full byte is received
+            uint8_t volume;
+            uint8_t play_pause = 0;
+            uint8_t nachste = 0;
+
+            volume      = data & 0x3F;
+            play_pause  = data & 0x80;
+            nachste     = data & 0x40;
+            
+            // system("clear");
+            std::cout << "Binary:" << std::endl;
+            std::cout << "    Total receive:" << std::bitset<8>(data) << std::endl;
+            std::cout << "    Volume:       " << std::bitset<8>(volume) << std::endl;
+            std::cout << "    Play/pause:   " << std::bitset<8>(play_pause) << std::endl;  
+            std::cout << "    Next:         " << std::bitset<8>(nachste) << std::endl;
+            std::cout << "Volume: " << static_cast<int>(volume * (MIX_MAX_VOLUME / 64)) << std::endl;  
+
+            ChangeVolume(volume); // Change volume
+
+            if (play_pause != 0) Pause_Resume();
+            if (nachste != 0) Next();
+
+            tcflush(serial_port, TCIFLUSH);
+        } 
+        if (tcgetattr(serial_port, &tty) < 0){
+            isConnected = false;
+            close(serial_port);
+        } 
+    }
+}
+
 
 int main() {
     if (SDL_Init(SDL_INIT_AUDIO) < 0) {
@@ -81,36 +124,13 @@ int main() {
     Mix_HookMusicFinished(Next);
     isPlaying = true;
 
-    // const char* port = "/dev/ttyUSB0"; // Adjust for your system
-    // int serial_port = open(port, O_RDWR);
-
-    // if (serial_port < 0) {
-    //     std::cerr << "Error opening UART\n";
-    //     return 1;
-    // }
-
-    // struct termios tty;
-    // if (tcgetattr(serial_port, &tty) < 0) {
-    //     std::cerr << "Error getting UART attributes\n";
-    //     close(serial_port);
-    //     return 1;
-    // }
-
-    // cfsetspeed(&tty, B115200); // Set baud rate
-    // if (tcsetattr(serial_port, TCSANOW, &tty) < 0) {
-    //     std::cerr << "Error setting UART attributes\n";
-    //     close(serial_port);
-    //     return 1;
-    // }
-
-    // // Create a separate thread for continuous UART reading
-    // std::thread uartThread(uartListener, serial_port);
-    // uartThread.detach(); // Run in background
+    // Create a separate thread for continuous UART reading
+    std::thread uartThread(uartListener);
+    uartThread.detach(); // Run in background
 
     // Keep the main program running
     char cmd;
     do {
-        std::cout << " >> Enter command: ";
         std::cin >> cmd;
         switch(cmd){
             case 'p':{
@@ -127,6 +147,5 @@ int main() {
         }
     } while (cmd != '0');
 
-    // close(serial_port);
     return 0;
 }
